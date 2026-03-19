cc381cf8adcb269b405e0ad67cfd60fd27361f52
David Boni <DAVIDB+amdocs@amdocs.com>
2026-03-20 01:18:01 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'read.tehilim',
+  appName: 'readTehilimApp-v2',
+  webDir: 'www'
+};
+
+export default config;
